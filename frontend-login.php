97c9aa99c9ad8a7819e2f33b1b159abe2aa40259
Damien Goehrig <damien.goehrig@gmail.com>
2022-01-06 09:30:15 -0500

    Add includes

--- frontend-login.php
+++ frontend-login.php
@@ -31,4 +31,32 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'FRONTEND_LOGIN_VERSION', '1.0.0' );
 
 
+// load templates name in page attributes
+function vive_ship_add_page_template ($templates) {
+    $templates['vm-acc.php'] = 'Vive Membership Account Page';
+    return $templates;
+    }
+add_filter ('theme_page_templates', 'vive_ship_add_page_template', 10, 1);
 
+// load page templates
+function vive_ship_load_plugin_template( $template ) {
+
+    if(  get_page_template_slug() === 'template-login.php' ) {
+
+        if ( $theme_file = locate_template( array( 'template-login.php' ) ) ) {
+            $template = $theme_file;
+        } else {
+            $template = plugin_dir_path( __DIR__ ) . 'frontend-login.php';
+        }
+    }
+
+    if($template == '') {
+        throw new \Exception('No template found');
+    }
+
+    return $template;
+}
+
+/* include hook file */
+require plugin_dir_path( __FILE__ ) . 'hooks.php';
+require plugin_dir_path( __FILE__ ) . 'rest.php';
